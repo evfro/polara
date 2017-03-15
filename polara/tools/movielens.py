838209f869af48fcee16b3ab960dcfdeb14f11ef
Evgeny Frolov <evfro@live.ru>
2017-03-15 23:05:52 +0300

    add new parameters to get_movielens_data function   it now allows to choose whether genres should be split or left as a long string   it also allows to get mapping to imdb and tmdb databases (only for latest datasets)

--- polara/tools/movielens.py
+++ polara/tools/movielens.py
@@ -4,50 +4,54 @@ from StringIO import StringIO
 from pandas.io.common import ZipFile
 
 
-def get_movielens_data(local_file=None, get_genres=False):
+def get_movielens_data(local_file=None, get_ratings=True, get_genres=False,
+                        split_genres=True, db_mapping=False):
     '''Downloads movielens data and stores it in pandas dataframe.
     '''
     if not local_file:
-        #print 'Downloading data...'
+        # downloading data
         zip_file_url = 'http://files.grouplens.org/datasets/movielens/ml-1m.zip'
         zip_response = get(zip_file_url)
         zip_contents = StringIO(zip_response.content)
-        #print 'Done.'
     else:
         zip_contents = local_file
 
-    #print 'Loading data into memory...'
+    ml_data = ml_genres = mapping = None
+    # loading data into memory
     with ZipFile(zip_contents) as zfile:
         zip_files = pd.Series(zfile.namelist())
         zip_file = zip_files[zip_files.str.contains('ratings')].iat[0]
-        zdata = zfile.read(zip_file)
-        if 'latest' in zip_file:
-            header = 0
-        else:
-            header = None
-        delimiter = ','
-        zdata = zdata.replace('::', delimiter) # makes data compatible with pandas c-engine
-        ml_data = pd.read_csv(StringIO(zdata), sep=delimiter, header=header, engine='c',
-                                names=['userid', 'movieid', 'rating', 'timestamp'],
-                                usecols=['userid', 'movieid', 'rating'])
+        is_latest = 'latest' in zip_file
+        header = 0 if is_latest else None
+        if get_ratings:
+            zdata = zfile.read(zip_file)
+            delimiter = ','
+            zdata = zdata.replace('::', delimiter) # makes data compatible with pandas c-engine
+            ml_data = pd.read_csv(StringIO(zdata), sep=delimiter, header=header, engine='c',
+                                    names=['userid', 'movieid', 'rating', 'timestamp'],
+                                    usecols=['userid', 'movieid', 'rating'])
 
         if get_genres:
             zip_file = zip_files[zip_files.str.contains('movies')].iat[0]
             with zfile.open(zip_file) as zdata:
-                if 'latest' in zip_file:
-                    delimiter = ','
-                else:
-                    delimiter = '::'
+                delimiter = ',' if is_latest else '::'
                 genres_data = pd.read_csv(zdata, sep=delimiter, header=header, engine='python',
                                             names=['movieid', 'movienm', 'genres'])
 
-            ml_genres = split_genres(genres_data)
-            ml_data = (ml_data, ml_genres)
+            ml_genres = get_split_genres(genres_data) if split_genres else genres_data
 
-    return ml_data
+        if is_latest and db_mapping:
+            # imdb and tmdb mapping - exists only in ml-latest datasets
+            zip_file = zip_files[zip_files.str.contains('links')].iat[0]
+            with zfile.open(zip_file) as zdata:
+                mapping = pd.read_csv(zdata, sep=',', header=0, engine='c',
+                                        names=['movieid', 'imdbid', 'tmdbid'])
+
+    res = [data for data in [ml_data, ml_genres, mapping] if data is not None]
+    return res
 
 
-def split_genres(genres_data):
+def get_split_genres(genres_data):
     genres_data.index.name = 'movie_idx'
     genres_stacked = genres_data.genres.str.split('|', expand=True).stack().to_frame('genreid')
     ml_genres = genres_data[['movieid', 'movienm']].join(genres_stacked).reset_index(drop=True)
