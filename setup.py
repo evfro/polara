964685ed1c3b9d6ae5fb99eee2fcc70d0f0ff03a
Evgeny Frolov <evfro@live.ru>
2017-08-21 19:21:33 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 
 _packages = ["polara",
             "polara/recommender",
+            "polara/recommender/coldstart",
             "polara/evaluation",
             "polara/lib",
             "polara/tools",
-            "polara/tools/mymedialite"]
+            "polara/tools/mymedialite",
+            "polara/tools/graphlab"]
 
 
 opts = dict(name="polara",
             description="Fast and flexible recommender framework",
             keywords = "recommender system",
-            version = "0.1",
+            version = "0.3.5",
             license="MIT",
             author="Evgeny Frolov",
             platforms=["any"],
