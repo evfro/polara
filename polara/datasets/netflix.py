d9fb8d20948e76d4df176d083e4284d3c99258ca
Evgeny Frolov <evfro@live.ru>
2018-10-06 20:55:23 +0300

    return int index for userid's in the Netflix dataset

--- polara/datasets/netflix.py
+++ polara/datasets/netflix.py
@@ -4,7 +4,7 @@ import tarfile
 
 def get_netflix_data(gz_file):
     movie_data = []
-    movie_name = []
+    movie_inds = []
     with tarfile.open(gz_file) as tar:
         training_data = tar.getmember('download/training_set.tar')
         with tarfile.open(fileobj=tar.extractfile(training_data)) as inner:
@@ -13,10 +13,10 @@ def get_netflix_data(gz_file):
                     f = inner.extractfile(item.name)
                     df = pd.read_csv(f)
                     movieid = df.columns[0]
-                    movie_name.append(movieid)
+                    movie_inds.append(int(movieid[:-1]))
                     movie_data.append(df[movieid])
 
-    data = pd.concat(movie_data, keys=movie_name)
+    data = pd.concat(movie_data, keys=movie_inds)
     data = data.reset_index().iloc[:, :3].rename(columns={'level_0': 'movieid',
                                                           'level_1': 'userid',
                                                           'level_2': 'rating'})
