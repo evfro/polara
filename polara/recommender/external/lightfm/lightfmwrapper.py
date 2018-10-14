1e26c78f1012feb69466816102b955ff44ab8464
Evgeny Frolov <evfro@live.ru>
2018-10-14 21:11:41 +0300

    avoid making unnecessary index copies in the LightFM's predict method
    also fixes missing user_features argument

--- polara/recommender/external/lightfm/lightfmwrapper.py
+++ polara/recommender/external/lightfm/lightfmwrapper.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 from lightfm import LightFM
 from polara.recommender.models import RecommenderModel
 from polara.lib.similarity import stack_features
@@ -69,12 +70,20 @@ class LightFMWrapper(RecommenderModel):
     def slice_recommendations(self, test_data, shape, start, stop, test_users=None):
         if self.data.warm_start:
             raise NotImplementedError
-        else:
-            slice_data = self._slice_test_data(test_data, start, stop)
-            all_items = self.data.index.itemid.new.values
-            n_users = stop - start
-            n_items = len(all_items)
-            scores = self._model.predict(np.repeat(test_users[start:stop], n_items),
-                                         np.tile(all_items, n_users),
-                                         item_features=self._item_features_csr).reshape(n_users, n_items)
+
+        slice_data = self._slice_test_data(test_data, start, stop)
+        all_items = self.data.index.itemid.new.values
+        n_users = stop - start
+        n_items = len(all_items)
+        # use stride tricks to avoid unnecessary copies of repeated indices
+        # have to conform with LightFM's dtype to avoid additional copies
+        itemsize = np.dtype('i4').itemsize
+        useridx = as_strided(test_users[start:stop].astype('i4', copy=False),
+                             (n_users, n_items), (itemsize, 0))
+        itemidx = as_strided(all_items.astype('i4', copy=False),
+                             (n_users, n_items), (0, itemsize))
+        scores = self._model.predict(useridx.ravel(), itemidx.ravel(),
+                                     user_features=self._user_features_csr,
+                                     item_features=self._item_features_csr
+                                     ).reshape(n_users, n_items)
         return scores, slice_data
