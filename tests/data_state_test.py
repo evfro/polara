a873043ce74dbac438d5bd1f7296afa3da2d5df6
Boris Shminke <boris@shminke.me>
2018-11-09 11:44:22 +0300

    add pipenv and fix some bugs in ipynb's

--- tests/data_state_test.py
+++ tests/data_state_test.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+from polara.recommender.data import RecommenderData
+
+
 def define_state(usn, hsz, trt):
     if usn:
         if (hsz>0) and (trt>0):
@@ -23,6 +28,7 @@ def assign_config(usn, hsz, trt, state):
     data_model._warm_start = usn
     data_model._state = state
 
+
 fields = ['userid', 'itemid', 'rating']
 data = pd.DataFrame(columns=fields)
 data_model = RecommenderData(data, *fields)
@@ -33,27 +39,27 @@ for usn in [False, True]:
             try:
                 state = define_state(usn, hsz, trt)
             except ValueError as e:
-                print '{}: usn {}, hsz {}, trt {}\n'.format(e, usn, hsz, trt)
+                print('{}: usn {}, hsz {}, trt {}\n'.format(e, usn, hsz, trt))
                 continue
-            print 'current config: usn - {}, hsz - {}, trt - {}, state - {}'.format(usn, hsz, trt, state)
+            print('current config: usn - {}, hsz - {}, trt - {}, state - {}'.format(usn, hsz, trt, state))
             assign_config(usn, hsz, trt, state)
 
             for usn_new in [False, True]:
                 for hsz_new in [0, 0.25, 1]:
                     for trt_new in [0, 0.1]:
-                        print 'usn: {:b}, hsz: {:4}, trt: {:4}'.format(usn_new, hsz_new, trt_new),
+                        print('usn: {:b}, hsz: {:4}, trt: {:4}'.format(usn_new, hsz_new, trt_new))
                         data_model.test_ratio = trt_new
                         data_model.holdout_size = hsz_new
                         data_model.warm_start = usn_new
                         try:
                             data_model._validate_config()
                         except ValueError as e:
-                            print e
+                            print(e)
                         else:
                             new_stt, upd = data_model._check_state_transition()
-                            print 'new state: {:3}, update rule: {}     '.format(new_stt, upd),
-                            print list(data_model._change_properties)
+                            print('new state: {:3}, update rule: {}     '.format(new_stt, upd),)
+                            print(list(data_model._change_properties))
                             
                         assign_config(usn, hsz, trt, state)
                         data_model._change_properties.clear()
-            print
+            print()
