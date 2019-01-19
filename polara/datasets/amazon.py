fa772e8a4abe0e9fba10720a0ebaadb8d240dbde
Evgeny Frolov <evfro@live.ru>
2019-01-19 15:00:45 +0300

    allow reading only first n rows in amazon data

--- polara/datasets/amazon.py
+++ polara/datasets/amazon.py
@@ -9,15 +9,16 @@ def parse_meta(path):
             yield literal_eval(line)
 
 
-def get_amazon_data(path=None, meta_path=None):
+def get_amazon_data(path=None, meta_path=None, nrows=None):
     res = []
     if path:
         data = pd.read_csv(path, header=None,
                            names=['userid', 'asin', 'rating', 'timestamp'],
-                           usecols=['userid', 'asin', 'rating'])
+                           usecols=['userid', 'asin', 'rating'],
+                           nrows=nrows)
         res.append(data)
     if meta_path:
-        meta = pd.DataFrame.from_records(parse_meta(meta_path))
+        meta = pd.DataFrame.from_records(parse_meta(meta_path), nrows=nrows)
         res.append(meta)
     if len(res) == 1:
         res = res[0]
